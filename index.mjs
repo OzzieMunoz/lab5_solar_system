0d1bbb2d5124e232d0cd0ea7ee5e307b8abdeefb
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-10-16 16:25:33 -0700

    solar system

---
+++ index.mjs
@@ -0,0 +1,29 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const solarSystem = (await import('npm-solarsystem')).default;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//root route
+app.get('/', async (req, res) => {
+   let url = "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&per_page=50&orientation=horizontal&q=solar%20system";
+   let response = await fetch(url);
+   let data = await response.json();
+   // console.log(data);
+   let randomImage = data.hit[0].webformatURL;
+   res.render('home.ejs', {data})
+});
+
+
+app.get('/planet', (req, res) => {
+   let planet_name = req.query.planetName;
+   let planetInfo = solarSystem[`get${planet_name}`]();
+   // console.log(planetInfo);
+   res.render('planetInfo.ejs', {planetInfo, planet_name});
+});
+
+app.listen(3000, () => {
+   console.log('server started');
+});
